b3f197bc1eb099656e31c91ac8f20a0edd2633d8
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2019-11-13 22:19:08 +0100

    Adapt Spawners

--- Scripts/Hole_Spawner.gd
+++ Scripts/Hole_Spawner.gd
@@ -3,14 +3,14 @@ extends Node2D
 var timer     = 0
 var timer_max = 10
 
-onready var hole = load( "res://Scenes/Rock.tscn" )
+onready var hole = load( "res://Scenes/Hole.tscn" )
 
 func _ready():
 	get_child(0).SPEED = 0
 
 func spawn():
 	var instance = hole.instance() 
-	instance.position = Vector2(position.x, 450)
+	instance.position = position
 	get_parent().add_child(instance)
 
 func _process(delta):
--- Scripts/Rock_Spawner.gd
+++ Scripts/Rock_Spawner.gd
@@ -1,21 +1,21 @@
 extends Node2D
 
 var timer     = 0
-var timer_max = 10
+var timer_max = 4
 
-onready var hole = load( "res://Scenes/Rock.tscn" )
+onready var rock = load( "res://Scenes/Rock.tscn" )
 
 func _ready():
 	get_child(0).SPEED = 0
 
 func spawn():
-	var instance = hole.instance() 
+	var instance = rock.instance() 
 	instance.position = position
-	#get_parent().add_child(instance)
+	get_parent().add_child(instance)
 
 func _process(delta):
 	timer += delta
 	if timer > timer_max:
 		spawn()
-		timer_max = randi()%12 + 5
+		timer_max = randi()%7 + 5
 		timer = 0
