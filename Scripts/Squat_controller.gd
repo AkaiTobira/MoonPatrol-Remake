a718d14f5eef50296fe6747064ae326db712cd74
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2019-11-27 23:15:12 +0100

    Bomber Up

--- Scripts/Squat_controller.gd
+++ Scripts/Squat_controller.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var active_squats = []
 var bomber_squats = []
+var bombs         = []
 
 func clear():
 	active_squats = []
 	bomber_squats = []
+	bombs         = []
 
 func is_squat_active(squat_id):
 	if len(active_squats) <= squat_id: return false
@@ -25,7 +27,7 @@ func is_sqaut_special_active( squat_id ):
 
 func bomber_destroyed( squat_id, numer_id ):
 	if len(active_squats) <= squat_id: return
-	SquatController.bomber_squats[squat_id][numer_id] = null
+	bomber_squats[squat_id][numer_id] = null
 
 func fill_squat( index, squat, valid ):
 	if valid != "Bomber": return
@@ -33,6 +35,16 @@ func fill_squat( index, squat, valid ):
 	squat[0].is_squat_lider = true
 	for alien in len(squat):
 		squat[alien].number_in_squat = alien+1
+
+func fire_bomb( index, bomb ):
+	if index > len( bombs ): return
+	bombs[index] = bomb
+	active_squats[index][1] = true
+
+func bomb_exploded( index ):
+	if index > len( bombs ): return
+	bombs[index] = null
+	active_squats[index][1] = false
 
 func _process(delta):
 	for i in len( active_squats ):
@@ -52,4 +64,5 @@ func register_new_squat(size_of_squat):
 			return i
 	active_squats.append([size_of_squat, false])
 	bomber_squats.append([])
+	bombs.append(null)
 	return len( active_squats) - 1
