5f34bfab06f421e429f09d7ee27622907de6660f
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2019-12-27 19:02:39 +0100

    Sounds I

---
+++ Scenes/Control.gd
@@ -0,0 +1,22 @@
+extends Control
+
+func update_score():
+	pass
+	
+func update_hi_score():
+	pass
+	
+func update_lives():
+	pass
+
+func update_main_distance():
+	pass
+
+func update_segment_distance():
+	pass
+	
+func show_warning(obstacle):
+	if obstacle == true:
+		$Warnings/ObstacleWarning.visible = true
+	
+	
